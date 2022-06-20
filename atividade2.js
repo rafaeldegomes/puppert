4c36e9c9054651d758a34f160c8957282923304b
rafaeldegomes <rafael.degomes@gmail.com>
2022-06-20 16:19:09 -0300

    atualização de correçao das atividades

--- atividade2.js
+++ atividade2.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+
   const browser = await puppeteer.launch({headless : false});
   const page = await browser.newPage();
   await page.goto('http://localhost/teste/atividade1.html');
@@ -10,32 +11,44 @@ const puppeteer = require('puppeteer');
   const body = await page.evaluate(() => {
     return {
       h1: $('h1'),
+      h2: $('h2'),
+      h3: $('h3'),
+      h4: $('h4'),
+      h5: $('h5'),
+      h6: $('h6'),
       p: $('p'), 
+      a: $('a'),
     };
   });
 
   var nota = 0;
   var media = 0;
-  var h1tamanho = body.h1.length;
-  var p = body.p.length;
 
-  //verifica a tag h1
-  if(h1tamanho == 1){
-    console.log('Voce usou apenas uma tag H1 e a atividade pede duas')
-    media= media +1 ;
-    nota = nota + 5;
-  }
-  else if(h1tamanho == 2){
-    console.log('Voce acertou a tag h1')
+  var p = body.p.length;
+  var a = body.a.length;
+  var h1tamanho = body.h1.length;
+  var h2tamanho = body.h2.length;
+  var h3tamanho = body.h3.length;
+  var h4tamanho = body.h4.length;
+  var h5tamanho = body.h5.length;
+  var h6tamanho = body.h6.length;
+  if(h1tamanho > 0 || 
+    h2tamanho > 0  ||
+    h3tamanho > 0 ||
+    h4tamanho > 0 ||
+    h5tamanho > 0 ||
+    h6tamanho > 0 ){
+    
     media= media +1 ;
     nota = nota + 10;
+    console.log('Voce acertou')
   }else{
-    console.log('voce nao utilizou a tag h1');
+    console.log('voce nao utilizou a tag h1, h2, h3, h4, h5 ou h6');
     media= media +1 ;
     nota = nota + 0;
   }
+  
 
-  //verifica a tag P 
   if(p == 1){
     console.log('Voce usou apenas uma tag P e a atividade pede duas')
     media= media +1 ;
@@ -45,14 +58,25 @@ const puppeteer = require('puppeteer');
     console.log('Voce acertou a tag P')
     media= media +1 ;
     nota = nota + 10;
+    
   }else{
     console.log('voce nao utilizou a tag P');
     media= media +1 ;
     nota = nota + 0;
   }
+
+  
+  if(a > 1){
+    console.log('voce acertou a tag de link');
+    media= media +1 ;
+    nota = nota + 10;
+  }else{
+    console.log('voce deve utilizar pelo duas tags para link ')
+    media= media +1 ;
+    nota = nota + 0;
+  }
   //console.log('H1:', body.h1.length)
  // console.log('P = ', body.p.length)
- //console.log( body)
  var nota_final = nota/media;
  console.log('------------ SUA NOTA ------------')
  console.log('Sua nota final na atividade é de = ', nota_final.toFixed(1))
---
+++ atividade3.js
@@ -0,0 +1,94 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+
+  const browser = await puppeteer.launch({headless : true});
+  const page = await browser.newPage();
+  await page.goto('http://localhost/teste/atividade1.html');
+  //await page.screenshot({ path: 'example.png' });
+
+ 
+  const body = await page.evaluate(() => {
+    return {
+      ul : $('ul'),
+      ol: $('ol'),
+      tabela: $('table'),
+      imagem: $('img'),
+      audio: $('audio'),
+      video: $('video')
+    };
+  });
+  var nota = 0;
+  var media = 0;
+
+
+  if(body.ul.length > 0 ){
+    console.log('ACERTOU O UL')
+    media= media +1 ;
+    nota = nota + 10;
+  }else{
+    console.log('** Errou O UL')
+    media= media +1 ;
+    nota = nota + 0;
+  }
+
+  if(body.ol.length > 0 ){
+    console.log('ACERTOU O OL')
+    media= media +1 ;
+    nota = nota + 10;
+  }else{
+    console.log('** Errou O OL')
+    media= media +1 ;
+    nota = nota + 0;
+  }
+
+  if(body.tabela.length > 0 ){
+    console.log('ACERTOU O TABELA')
+    media= media +1 ;
+    nota = nota + 10;
+  }else{
+    console.log('** Errou O TABELA')
+    media= media +1 ;
+    nota = nota + 0;
+  }
+
+  if(body.imagem.length == 1 ){
+    console.log('VOCE DEVE COLOCAR MAIS DE UMA IMAGEM EM SUA PAGINA')
+    media= media +1 ;
+    nota = nota + 10;
+  }else if(body.imagem.length > 1){
+    console.log('Acertou a IMAGEM')
+    media= media +1 ;
+    nota = nota + 5;
+  }
+  else{
+    console.log('** Errou O IMAGEM')
+    media= media +1 ;
+    nota = nota + 0;
+  }
+
+  if(body.audio.length > 0 ){
+    console.log('ACERTOU O AUDIO')
+    media= media +1 ;
+    nota = nota + 10;
+  }else{
+    console.log('** Errou O AUDIO')
+    media= media +1 ;
+    nota = nota + 0;
+  }
+
+  if(body.video.length > 0 ){
+    console.log('ACERTOU O VIDEO')
+    media= media +1 ;
+    nota = nota + 10;
+  }else{
+    console.log('** Errou O VIDEO')
+    media= media +1 ;
+    nota = nota + 0;
+  }
+
+  var nota_final = nota/media;
+  console.log('------------ SUA NOTA ------------')
+  console.log('Sua nota final na atividade é de = ', nota_final.toFixed(1))
+  await browser.close();
+})();
